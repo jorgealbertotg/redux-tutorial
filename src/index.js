515cda3de61b209a09b7ad5cc9c31472347d028a
jota <jorge-alberto.tolentino-garcia@gft.com>
2020-07-20 11:10:56 -0500

    Adding middleware

--- src/index.js
+++ src/index.js
@@ -1,17 +1,103 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+const mathReducer = (state = {
+  result: 1,
+  lastValues: []
+}, action) => {
+  switch (action.type) {
+    case 'ADD':
+      state = {
+        ...state,
+        result: state.result + action.payload,
+        lastValues: [...state.lastValues, action.payload]
+      };
+      break;
+    case 'SUBSTRACT':
+      state = {
+        ...state,
+        result: state.result - action.payload,
+        lastValues: [...state.lastValues, action.payload]
+      };
+      break;
+    default:
+      return state;
+  }
+  return state;
+}
+
+const userReducer = (state = {
+  name: 'Tole',
+  age: 31
+}, action) => {
+  switch (action.type) {
+    case 'SET_NAME':
+      state = {
+        ...state,
+        name: action.payload
+      };
+      break;
+    case 'SET_AGE':
+      state = {
+        ...state,
+        age: action.payload
+      };
+      break;
+    default:
+      return state;
+  }
+  return state;
+}
+
+const myLogger = (store) => (next) => (action) => {
+  //console.log('Logged Action', action);
+  next(action);
+}
+
+const store = createStore(
+    combineReducers({mathReducer, userReducer}),
+    {}, 
+    applyMiddleware(myLogger, logger)
+  );
+
+store.subscribe(() => {
+  //console.log('Store updated', store.getState());
+});
+
+store.dispatch({
+  type: 'ADD',
+  payload: 100
+});
+
+store.dispatch({
+  type: 'ADD',
+  payload: 22
+});
+
+store.dispatch({
+  type: 'SUBSTRACT',
+  payload: 80
+});
+
+store.dispatch({
+  type: 'SET_AGE',
+  payload: 100
+});
